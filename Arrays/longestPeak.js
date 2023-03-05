ba751e1f19413c9698bfa80a53a5667c3715b00d
Sajramkisho <61645278+Sajramkisho@users.noreply.github.com>
2023-03-05 08:21:07 +0530

    LongestPeak

--- Arrays/longestPeak.js
+++ Arrays/longestPeak.js
@@ -1,32 +1,22 @@
-// time: O(N^2) | space: O(1)
+// time: O(N) | space: O(1)
 const longestPeak = (arr) => {
-  if (arr.length <= 2) return
-
-  let count
   let maxCount = Number.MIN_VALUE
-  for (let i = 0; i < arr.length; i++) {
-    count = 0
-    if (i == 0) continue
-    else {
-      let left = i - 1
-      let right = i + 1
-      while (left >= 0 || right < arr.length) {
-        if (arr[i] > arr[left] && arr[i] > arr[right]) {
-          count = 3
-          left--
-          right++
-        } else if (arr[i] > arr[left] && arr[i] <= arr[right]) {
-          count++
-          left--
-        } else if (arr[i] <= arr[left] && arr[i] > arr[right]) {
-          count++
-          right++
-        } else break
-      }
-    }
+  let count = 0
+  for (let i = 1; i < arr.length - 1; i++) {
+    let isPeak = arr[i - 1] < arr[i] && arr[i + 1] < arr[i]
+    if (!isPeak) continue
+
+    //isPeak === true
+    let L = i - 2
+    while (L >= 0 && arr[L] < arr[L + 1]) L--
+
+    let R = i + 2
+    while (R < arr.length && arr[R] < arr[R - 1]) R++
+
+    count = R - L - 1
     maxCount = Math.max(count, maxCount)
   }
-  return maxCount
+  console.log(maxCount)
 }
 
-console.log(longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]))
+longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3])
